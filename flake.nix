20e3bac66c9923ffb0496a27d2f2677a6f73fb2d
Tiffany Tran <tiffany@Tiffanys-MacBook-Air-3.local>
2024-06-30 12:04:22 -0700

    chagned somethinbg

--- flake.nix
+++ flake.nix
@@ -10,7 +10,6 @@
     inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
-        "x86_64-linux"
         "x86_64-darwin"
         "aarch64-linux"
         "aarch64-darwin"
