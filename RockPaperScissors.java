ba0513bcac2c83692595ee5a300eded3e06589f8
Tyffoni <167036717+Tyffoni@users.noreply.github.com>
2024-08-18 12:56:04 -0700

    Update RockPaperScissors.java

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -1,17 +1,6 @@
-package Test;
-
 import java.io.Console;
 
 public class RockPaperScissors {
-    public static void main(String[] args){
-        RockPaperScissors rps = new RockPaperScissors();
-        String computerAnswer = rps.getComputerAnswer();
-        String myAnswer = rps.getMyAnswer();
-        System.out.println("Your move is " + myAnswer);
-        System.out.println("Computer's move is " + computerAnswer);
-        rps.game(computerAnswer, myAnswer);
-    }
-
     public String getComputerAnswer(){
         int num = (int)(Math.random() * 3);
         String answer = "";
